89b1a033ab90a31e7e53e687fdde30c2afe3d3b1
Lele Chen <lelechen@fb.com>
2021-06-06 11:56:04 -0400

    g

--- model/model.py
+++ model/model.py
@@ -328,16 +328,16 @@ class TexMeshModule(pl.LightningModule):
         #mesh loss
         loss_mesh = 0
         loss_mesh += self.l1loss(rec_mesh_A, batch['Amesh'])* self.opt.lambda_mesh
-        loss_mesh += self.l1loss(rec_mesh_B, batch['Btex'])* self.opt.lambda_mesh
+        loss_mesh += self.l1loss(rec_mesh_B, batch['Bmesh'])* self.opt.lambda_mesh
         # mismatch loss
         if not self.opt.no_mismatch_loss:
             for i in range(map_type.shape[0]):
                 if map_type[i] == 0: # same id, diff exp, mismatch is decided by exp
                     loss_mesh += self.l1loss(rec_mesh_AB[i].unsqueeze(0), batch['Btex'][i].unsqueeze(0)) * self.opt.lambda_mesh* self.opt.lambda_mismatch
-                    loss_mesh += self.l1loss(rec_mesh_BA[i].unsqueeze(0), batch['Atex'][i].unsqueeze(0)) * self.opt.lambda_mesh* self.opt.lambda_mismatch
+                    loss_mesh += self.l1loss(rec_mesh_BA[i].unsqueeze(0), batch['Bmesh'][i].unsqueeze(0)) * self.opt.lambda_mesh* self.opt.lambda_mismatch
                 else:
                     loss_mesh += self.l1loss(rec_mesh_AB[i].unsqueeze(0), batch['Atex'][i].unsqueeze(0)) * self.opt.lambda_mesh* self.opt.lambda_mismatch
-                    loss_mesh += self.l1loss(rec_mesh_BA[i].unsqueeze(0), batch['Btex'][i].unsqueeze(0)) * self.opt.lambda_mesh* self.opt.lambda_mismatch
+                    loss_mesh += self.l1loss(rec_mesh_BA[i].unsqueeze(0), batch['Bmesh'][i].unsqueeze(0)) * self.opt.lambda_mesh* self.opt.lambda_mismatch
         
         # adversarial loss is binary cross-entropy
         g_loss = loss_G_pix + loss_G_VGG + loss_G_CLS + loss_mesh
