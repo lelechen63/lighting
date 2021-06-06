0de89a21c413fc7b6ca2ab31f052ebe2df7d401c
Lele Chen <lelechen@fb.com>
2021-06-06 14:36:22 -0400

    g

--- model/model.py
+++ model/model.py
@@ -267,6 +267,7 @@ class TexMeshModule(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         self.batch = batch
+        self.batch_idx = batch_idx
         # train generator
         # generate images
         rec_tex_A, rec_mesh_A, rec_tex_B, rec_mesh_B, \
