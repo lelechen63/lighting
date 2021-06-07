71b2ecddcf844c373085b7a7adc2b2e47e8f9ab5
Lele Chen <lelechen@fb.com>
2021-06-06 23:58:52 -0400

    g

--- data/facescape.py
+++ data/facescape.py
@@ -235,8 +235,8 @@ class FacescapeMeshTexDataset(torch.utils.data.Dataset):
             om_mesh = openmesh.read_trimesh(mesh_path)
             A_vertices = np.array(om_mesh.points()).reshape(-1)
             self.total_tex[data].append(A_vertices)
-            if len(self.total_tex) == 65:
-                break
+            # if len(self.total_tex) == 65:
+            #     break
         
     def __getitem__(self, index):
         t = time.time()
