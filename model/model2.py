7f7d558ebd1c89e0a1562f5e8e7074fa73587c69
Lele Chen <lelechen@fb.com>
2021-06-08 23:55:29 -0400

    g

--- model/model2.py
+++ model/model2.py
@@ -383,7 +383,7 @@ class TexMeshModule(pl.LightningModule):
                 gg = gg.numpy()
                 gg = torch.from_numpy(gg.astype(np.float32))
 
-                gt_Amesh = self.meshrender.meshrender(int(tmp[0]), int(tmp[-1].split('_')[0]),batch['Amesh'].gg.cpu() )
+                gt_Amesh = self.meshrender.meshrender(int(tmp[0]), int(tmp[-1].split('_')[0]),gg )
                 rec_Amesh = self.meshrender.meshrender(int(tmp[0]), int(tmp[-1].split('_')[0]), rec_mesh_A.data[0].cpu())
 
 
