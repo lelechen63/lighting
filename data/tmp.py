a17a810cc54d919090c5ce5c8c577d80629e2cf2
Lele Chen <lelechen@fb.com>
2021-06-09 00:05:01 -0400

    g

--- data/tmp.py
+++ data/tmp.py
@@ -3,6 +3,8 @@ import os
 
 base = '/raid/celong/FaceScape/fsmview_images'
 newbase = '/raid/celong/FaceScape/jsons'
+if not os.path.exists(  newbase):
+        os.mkdir( newbase )
 for id in os.listdir(base):
     if not os.path.exists(  newbase + '/' + id ):
         os.mkdir( newbase + '/' + id)
