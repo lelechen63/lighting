6e81ec56ec72c269b47b4724a48a80be98598778
Lele Chen <lelechen@fb.com>
2021-06-09 00:04:44 -0400

    g

---
+++ data/tmp.py
@@ -0,0 +1,13 @@
+import os 
+
+
+base = '/raid/celong/FaceScape/fsmview_images'
+newbase = '/raid/celong/FaceScape/jsons'
+for id in os.listdir(base):
+    if not os.path.exists(  newbase + '/' + id ):
+        os.mkdir( newbase + '/' + id)
+    for exp in os.listdir( base + '/' + id ):
+        if not os.path.exists(  newbase + '/' + id  + '/' + exp):
+            os.mkdir( newbase + '/' + id + '/' + exp)
+        command ='cp  ' +  newbase + '/' + id  + '/' + exp +'params.json' + ' ' + newbase + '/' + id + '/' + exp
+        os.system(command)
