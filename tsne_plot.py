8af1cceedd6d9c07187e8e7797644a6ba0488d34
Lele Chen <lelechen@fb.com>
2021-07-21 15:21:21 -0400

    g

--- tsne_plot.py
+++ tsne_plot.py
@@ -49,7 +49,7 @@ X = np.vstack([digits.data[digits.target==i]
                for i in range(10)])
 y = np.hstack([digits.target[digits.target==i]
                for i in range(10)])
-x = X[:100]
+X = X[:100]
 y =y[:100]
 print (X.shape,y.shape)
 digits_proj = TSNE(random_state=RS).fit_transform(X)
