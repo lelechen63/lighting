e097d1291055f906a6cb6fc2050b12568ba6eae8
lelechen63 <lchen63@cs.rochester.edu>
2021-07-28 14:58:52 -0400

    add color debug

--- debug.py
+++ debug.py
@@ -16,7 +16,12 @@ def np_normalize(x, low= 0, high = 1):
     # input x: size 256x256x3, output: 245x256x3
     if x.shape[0] != 3:
         x = x.transpose(2, 0, 1)
-    x_max = x.reshape(*x.shape[:-2], -1).max(axis=-1)[0][..., None, None]
+    print (x.shape)
+    x_max = x.reshape(*x.shape[:-2], -1)
+    print (x_max.shape)
+	x_max =x_max.max(axis=-1)[0]
+	print (x_max.shape)
+	x_max = x_max[..., None, None]
     x_min = x.view(*x.shape[:-2], -1).min(axis=-1)[0][..., None, None]
     scale = (high - low) / (x_max - x_min)
 
