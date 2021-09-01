e6d1a6289656a1223c909ff8964de0e8ad3a8f61
Lele Chen <lelechen@dhcp-10-5-6-53.wireless.rochester.edu>
2021-09-01 10:05:56 -0400

    g

--- debug.py
+++ debug.py
@@ -80,16 +80,14 @@ def gallery(array, ncols=3):
     return result
 
 seq = iaa.Sequential([
-    iaa.GaussianBlur(sigma=(0, 3.0)),
     # Strengthen or weaken the contrast in each image.
     iaa.LinearContrast((0.75, 1.5)),
-    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
     # Make some images brighter and some darker.
     # In 20% of all cases, we sample the multiplier once per channel,
     # which can end up changing the color of the images.
-    iaa.Multiply((0.8, 1.2), per_channel=0.2),
-    iaa.AddToBrightness((-30, 30)),
-    iaa.WithHueAndSaturation(iaa.WithChannels(0, iaa.Add((0, 50))))
+    # iaa.Multiply((0.8, 1.2), per_channel=0.2),
+    # iaa.AddToBrightness((-30, 30)),
+    # iaa.WithHueAndSaturation(iaa.WithChannels(0, iaa.Add((0, 50))))
 ])
 
 print ('1111')
