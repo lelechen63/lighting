0468ef4f7bc5fc369457cfabff3ba92e35a9069f
Lele Chen <lelechen@dhcp-10-5-6-53.wireless.rochester.edu>
2021-09-01 17:00:06 -0400

    g

--- data/facescape.py
+++ data/facescape.py
@@ -503,9 +503,10 @@ class FacescapeMeshTexDataset(torch.utils.data.Dataset):
         tex = np.expand_dims(tex, axis=0)
         tex =  self.augseq( images = tex )
         tex = tex.astype(np.float64)[0]
-        tex = (tex - self.meantex)/self.stdtex
+        cv2.imwrite('gg2.png', tex)
+        tex_tensor = (tex - self.meantex)/self.stdtex
        
-        tex_tensor = torch.FloatTensor(tex).permute(2,0,1)
+        tex_tensor = torch.FloatTensor(tex_tensor).permute(2,0,1)
         A_vertices = self.total_tex[self.data_list[index]][1]
         A_vertices = A_vertices.reshape(-1,3)
             
