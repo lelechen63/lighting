d9b73066aa0b99c64de4e9f4e86cacd1a6209089
Lele Chen <lelechen@dhcp-10-5-6-53.wireless.rochester.edu>
2021-09-01 21:08:05 -0400

    g

--- debug.py
+++ debug.py
@@ -3,7 +3,7 @@ import cv2
 import torch 
 import time
 from imgaug import augmenters as iaa
-
+import random
 
 
 def np_normalize(x, low= 0, high = 1):
@@ -85,6 +85,174 @@ seq = iaa.Sequential([
     iaa.WithHueAndSaturation(iaa.WithChannels(0, iaa.Add((0, 50))))
 ])
 
+
+def apply_lut(image, table):
+    """Map an input image to a new one using a lookup table.
+    Added in 0.4.0.
+    **Supported dtypes**:
+        See :func:`~imgaug.imgaug.apply_lut_`.
+    Parameters
+    ----------
+    image : ndarray
+        See :func:`~imgaug.imgaug.apply_lut_`.
+    table : ndarray or list of ndarray
+        See :func:`~imgaug.imgaug.apply_lut_`.
+    Returns
+    -------
+    ndarray
+        Image after mapping via lookup table.
+    """
+    return apply_lut_(np.copy(image), table)
+
+
+def _normalize_cv2_input_arr_(arr):
+    flags = arr.flags
+    if not flags["OWNDATA"]:
+        arr = np.copy(arr)
+        flags = arr.flags
+    if not flags["C_CONTIGUOUS"]:
+        arr = np.ascontiguousarray(arr)
+    return arr
+
+
+def apply_lut_(image, table):
+    """Map an input image in-place to a new one using a lookup table.
+    Added in 0.4.0.
+    **Supported dtypes**:
+        * ``uint8``: yes; fully tested
+        * ``uint16``: no
+        * ``uint32``: no
+        * ``uint64``: no
+        * ``int8``: no
+        * ``int16``: no
+        * ``int32``: no
+        * ``int64``: no
+        * ``float16``: no
+        * ``float32``: no
+        * ``float64``: no
+        * ``float128``: no
+        * ``bool``: no
+    Parameters
+    ----------
+    image : ndarray
+        Image of dtype ``uint8`` and shape ``(H,W)`` or ``(H,W,C)``.
+    table : ndarray or list of ndarray
+        Table of dtype ``uint8`` containing the mapping from old to new
+        values. Either a ``list`` of ``C`` ``(256,)`` arrays or a single
+        array of shape ``(256,)`` or ``(256, C)`` or ``(1, 256, C)``.
+        In case of ``(256,)`` the same table is used for all channels,
+        otherwise a channelwise table is used and ``C`` is expected to match
+        the number of channels.
+    Returns
+    -------
+    ndarray
+        Image after mapping via lookup table.
+        This *might* be the same array instance as provided via `image`.
+    """
+
+    image_shape_orig = image.shape
+    nb_channels = 1 if len(image_shape_orig) == 2 else image_shape_orig[-1]
+
+    if 0 in image_shape_orig:
+        return image
+
+    image = _normalize_cv2_input_arr_(image)
+
+    # [(256,), (256,), ...] => (256, C)
+    if isinstance(table, list):
+        assert len(table) == nb_channels, (
+            "Expected to get %d tables (one per channel), got %d instead." % (
+                nb_channels, len(table)))
+        table = np.stack(table, axis=-1)
+
+    # (256, C) => (1, 256, C)
+    if table.shape == (256, nb_channels):
+        table = table[np.newaxis, :, :]
+
+    assert table.shape == (256,) or table.shape == (1, 256, nb_channels), (
+        "Expected 'table' to be any of the following: "
+        "A list of C (256,) arrays, an array of shape (256,), an array of "
+        "shape (256, C), an array of shape (1, 256, C). Transformed 'table' "
+        "up to shape %s for image with shape %s (C=%d)." % (
+            table.shape, image_shape_orig, nb_channels))
+
+    if nb_channels > 512:
+        if table.shape == (256,):
+            table = np.tile(table[np.newaxis, :, np.newaxis],
+                            (1, 1, nb_channels))
+
+        subluts = []
+        for group_idx in np.arange(int(np.ceil(nb_channels / 512))):
+            c_start = group_idx * 512
+            c_end = c_start + 512
+            subluts.append(apply_lut_(image[:, :, c_start:c_end],
+                                      table[:, :, c_start:c_end]))
+
+        return np.concatenate(subluts, axis=2)
+
+    assert image.dtype == _UINT8_DTYPE, (
+        "Expected uint8 image, got dtype %s." % (image.dtype.name,))
+
+    image = cv2.LUT(image, table, dst=image)
+    return image
+
+
+
+def adjust_contrast_linear(arr, alpha):
+    """Adjust contrast by scaling each pixel to ``127 + alpha*(v-127)``.
+    **Supported dtypes**:
+        * ``uint8``: yes; fully tested (1) (2)
+        * ``uint16``: yes; tested (2)
+        * ``uint32``: yes; tested (2)
+        * ``uint64``: no (3)
+        * ``int8``: yes; tested (2)
+        * ``int16``: yes; tested (2)
+        * ``int32``: yes; tested (2)
+        * ``int64``: no (2)
+        * ``float16``: yes; tested (2)
+        * ``float32``: yes; tested (2)
+        * ``float64``: yes; tested (2)
+        * ``float128``: no (2)
+        * ``bool``: no (4)
+        - (1) Handled by ``cv2``. Other dtypes are handled by raw ``numpy``.
+        - (2) Only tested for reasonable alphas with up to a value of
+              around ``100``.
+        - (3) Conversion to ``float64`` is done during augmentation, hence
+              ``uint64``, ``int64``, and ``float128`` support cannot be
+              guaranteed.
+        - (4) Does not make sense for contrast adjustments.
+    Parameters
+    ----------
+    arr : numpy.ndarray
+        Array for which to adjust the contrast. Dtype ``uint8`` is fastest.
+    alpha : number
+        Multiplier to linearly pronounce (``>1.0``), dampen (``0.0`` to
+        ``1.0``) or invert (``<0.0``) the difference between each pixel value
+        and the dtype's center value, e.g. ``127`` for ``uint8``.
+    Returns
+    -------
+    numpy.ndarray
+        Array with adjusted contrast.
+    """
+
+    # int8 is also possible according to docs
+    # but here it seemed like `d` was 0 for CV_8S, causing that to fail
+    iinfo = np.iinfo(arry.dtype)
+
+    min_value, center_value, max_value = iinfo.min, iinfo.min + 0.5 * iinfo.max, iinfo.max
+    # TODO get rid of this int(...)
+    center_value = int(center_value)
+
+    value_range = np.arange(0, 256, dtype=np.float32)
+
+    # 127 + alpha*(I_ij-127)
+    # using np.float32(.) here still works when the input is a numpy array
+    # of size 1
+    alpha = np.float32(alpha)
+    table = center_value + alpha * (value_range - center_value)
+    table = np.clip(table, min_value, max_value).astype(arr.dtype)
+    arr_aug = ia.apply_lut(arr, table)
+    return arr_aug
 print ('1111')
 img = cv2.imread('/data/home/uss00022/lelechen/data/Facescape/textured_meshes/1/models_reg/10_dimpler.jpg')
 print ('1111')
@@ -103,7 +271,10 @@ t = time.time()
 images_aug = []
 print ('1111')
 for i in range(64):
-    images_aug.append( seq(images=imgs)[0])
+    alpha = random.uniform(0.75, 1.5)
+    im = adjust_contrast_linear(imgs, alpha)
+    images_aug.append( im[0])
+    # images_aug.append( seq(images=imgs)[0])
 images_aug = np.asarray(images_aug)
 print ('1111')
 
