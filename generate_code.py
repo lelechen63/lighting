8b81bb099404681a90fbc72ace3d395842df6825
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2021-12-13 16:42:38 -0500

    g

--- generate_code.py
+++ generate_code.py
@@ -58,7 +58,7 @@ def main():
             rec_mesh_A, code = module( batch['Amesh'].view(batch['Amesh'].shape[0], -1, 3).to(device))
 
             code = Encoder( batch['Amesh'].view(batch['Amesh'].shape[0], -1, 3).to(device))
-            save_p = os.path.join(opt.dataroot '/meshcode/',  batch['A_path'][0] + '_mesh.npy')
+            save_p = os.path.join(opt.dataroot +  '/meshcode/',  batch['A_path'][0] + '_mesh.npy')
             tmp =  batch['A_path'][0].split('/')
             os.makedirs(  os.path.join(opt.dataroot + '/meshcode/',  tmp[0], tmp[1]), exist_ok =True)
             np.save( save_p, code.detach().cpu().numpy())
@@ -72,7 +72,7 @@ def main():
             rec_mesh_A, code = module( batch['Amesh'].view(batch['Amesh'].shape[0], -1, 3).to(device))
 
             code = Encoder( batch['Amesh'].view(batch['Amesh'].shape[0], -1, 3).to(device))
-            save_p = os.path.join(opt.dataroot '/meshcode/',  batch['A_path'][0] + '_mesh.npy')
+            save_p = os.path.join(opt.dataroot + '/meshcode/',  batch['A_path'][0] + '_mesh.npy')
             tmp =  batch['A_path'][0].split('/')
             os.makedirs(  os.path.join(opt.dataroot + '/meshcode/',  tmp[0], tmp[1]), exist_ok =True)
             np.save( save_p, code.detach().cpu().numpy())
