688e405bac3768cdd53f32301ccf8a120b5720e5
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2021-12-17 11:36:30 -0500

    g

--- util/facetools/align/ffhq-align.py
+++ util/facetools/align/ffhq-align.py
@@ -1,101 +1,153 @@
-import os
+"""
+brief: face alignment with FFHQ method (https://github.com/NVlabs/ffhq-dataset)
+author: lzhbrian (https://lzhbrian.me)
+date: 2020.1.5
+note: code is heavily borrowed from 
+    https://github.com/NVlabs/ffhq-dataset
+    http://dlib.net/face_landmark_detection.py.html
+requirements:
+    apt install cmake
+    conda install Pillow numpy scipy
+    pip install dlib
+    # download face landmark model from: 
+    # http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+"""
 
 import numpy as np
-import argparse
-import scipy.ndimage
+import PIL
 import PIL.Image
-import face_alignment
-from torch.autograd.grad_mode import enable_grad
+import sys
+import os
+import glob
+import scipy
+import scipy.ndimage
+import dlib
 
 
-def image_align(src_file, dst_file, face_landmarks, output_size=256, transform_size=1024, enable_padding=True):
-        # Align function from FFHQ dataset pre-processing step
-        # https://github.com/NVlabs/ffhq-dataset/blob/master/download_ffhq.py
+# download model from: http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+predictor = dlib.shape_predictor('/home/uss00022/lelechen/basic/shape_predictor_68_face_landmarks.dat')
 
-        lm = np.array(face_landmarks)
-        lm_chin          = lm[0  : 17, :2]  # left-right
-        lm_eyebrow_left  = lm[17 : 22, :2]  # left-right
-        lm_eyebrow_right = lm[22 : 27, :2]  # left-right
-        lm_nose          = lm[27 : 31, :2]  # top-down
-        lm_nostrils      = lm[31 : 36, :2]  # top-down
-        lm_eye_left      = lm[36 : 42, :2]  # left-clockwise
-        lm_eye_right     = lm[42 : 48, :2]  # left-clockwise
-        lm_mouth_outer   = lm[48 : 60, :2]  # left-clockwise
-        lm_mouth_inner   = lm[60 : 68, :2]  # left-clockwise
+def get_landmark(filepath):
+    """get landmark with dlib
+    :return: np.array shape=(68, 2)
+    """
+    detector = dlib.get_frontal_face_detector()
 
-        # Calculate auxiliary vectors.
-        eye_left     = np.mean(lm_eye_left, axis=0)
-        eye_right    = np.mean(lm_eye_right, axis=0)
-        eye_avg      = (eye_left + eye_right) * 0.5
-        eye_to_eye   = eye_right - eye_left
-        mouth_left   = lm_mouth_outer[0]
-        mouth_right  = lm_mouth_outer[6]
-        mouth_avg    = (mouth_left + mouth_right) * 0.5
-        eye_to_mouth = mouth_avg - eye_avg
+    img = dlib.load_rgb_image(filepath)
+    dets = detector(img, 1)
 
-        # Choose oriented crop rectangle.
-        x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
-        x /= np.hypot(*x)
-        x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
-        y = np.flipud(x) * [-1, 1]
-        c = eye_avg + eye_to_mouth * 0.1
-        quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
-        qsize = np.hypot(*x) * 2
+    print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0), shape.part(1)))
 
-        # Load in-the-wild image.
-        if not os.path.isfile(src_file):
-            print('\nCannot find source image. Please run "--wilds" before "--align".')
-            return
-        img = PIL.Image.open(src_file)
 
-        # Shrink.
-        shrink = int(np.floor(qsize / output_size * 0.5))
-        if shrink > 1:
-            rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
-            img = img.resize(rsize, PIL.Image.ANTIALIAS)
-            quad /= shrink
-            qsize /= shrink
+    t = list(shape.parts())
+    a = []
+    for tt in t:
+        a.append([tt.x, tt.y])
+    lm = np.array(a)
+    # lm is a shape=(68,2) np.array
+    return lm
 
-        # Crop.
-        border = max(int(np.rint(qsize * 0.1)), 3)
-        crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
-        crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
-        if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
-            img = img.crop(crop)
-            quad -= crop[0:2]
+    
+def align_face(filepath):
+    """
+    :param filepath: str
+    :return: PIL Image
+    """
 
-        # Pad.
-        pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
-        pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
-        if enable_padding and max(pad) > border - 4:
-            pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
-            img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
-            h, w, _ = img.shape
-            y, x, _ = np.ogrid[:h, :w, :1]
-            mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
-            blur = qsize * 0.02
-            img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
-            img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
-            img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
-            quad += pad[:2]
+    lm = get_landmark(filepath)
+    
+    lm_chin          = lm[0  : 17]  # left-right
+    lm_eyebrow_left  = lm[17 : 22]  # left-right
+    lm_eyebrow_right = lm[22 : 27]  # left-right
+    lm_nose          = lm[27 : 31]  # top-down
+    lm_nostrils      = lm[31 : 36]  # top-down
+    lm_eye_left      = lm[36 : 42]  # left-clockwise
+    lm_eye_right     = lm[42 : 48]  # left-clockwise
+    lm_mouth_outer   = lm[48 : 60]  # left-clockwise
+    lm_mouth_inner   = lm[60 : 68]  # left-clockwise
 
-        # Transform.
-        img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
-        if output_size < transform_size:
-            img = img.resize((output_size, output_size), PIL.Image.ANTIALIAS)
+    # Calculate auxiliary vectors.
+    eye_left     = np.mean(lm_eye_left, axis=0)
+    eye_right    = np.mean(lm_eye_right, axis=0)
+    eye_avg      = (eye_left + eye_right) * 0.5
+    eye_to_eye   = eye_right - eye_left
+    mouth_left   = lm_mouth_outer[0]
+    mouth_right  = lm_mouth_outer[6]
+    mouth_avg    = (mouth_left + mouth_right) * 0.5
+    eye_to_mouth = mouth_avg - eye_avg
 
-        # Save aligned image.
-        img.save(dst_file, 'PNG')
+    # Choose oriented crop rectangle.
+    x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
+    x /= np.hypot(*x)
+    x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
+    y = np.flipud(x) * [-1, 1]
+    c = eye_avg + eye_to_mouth * 0.1
+    quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
+    qsize = np.hypot(*x) * 2
+
+
+    # read image
+    img = PIL.Image.open(filepath)
+
+    output_size=1024
+    transform_size=4096
+    enable_padding=True
+
+    # Shrink.
+    shrink = int(np.floor(qsize / output_size * 0.5))
+    if shrink > 1:
+        rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
+        img = img.resize(rsize, PIL.Image.ANTIALIAS)
+        quad /= shrink
+        qsize /= shrink
+
+    # Crop.
+    border = max(int(np.rint(qsize * 0.1)), 3)
+    crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+    crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
+    if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
+        img = img.crop(crop)
+        quad -= crop[0:2]
+
+    # Pad.
+    pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+    pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
+    if enable_padding and max(pad) > border - 4:
+        pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
+        img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+        h, w, _ = img.shape
+        y, x, _ = np.ogrid[:h, :w, :1]
+        mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
+        blur = qsize * 0.02
+        img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+        img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
+        img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+        quad += pad[:2]
+
+    # Transform.
+    img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+    if output_size < transform_size:
+        img = img.resize((output_size, output_size), PIL.Image.ANTIALIAS)
+
+    # Save aligned image.
+    return img
 
 def main():
-    landmarks_detector = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
+    # landmarks_detector = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
     img_p = '/nfs/STG/CodecAvatar/lelechen/Facescape/fsmview_images/307/1_neutral/1.jpg'
-    face_landmarks  = landmarks_detector.get_landmarks(img_p)[0]
-    output_size = 1024
-    transform_size = 4096
-    no_padding = False
-    image_align(img_p, './tmp.png', face_landmarks, output_size, transform_size, no_padding)
-
+    # face_landmarks  = landmarks_detector.get_landmarks(img_p)[0]
+    # output_size = 1024
+    # transform_size = 4096
+    # no_padding = False
+    # image_align(img_p, './tmp.png', face_landmarks, output_size, transform_size, no_padding)
+    img = align_face(img_p)
+    img.save('./tmp.png', 'PNG')
 main()
 # if __name__ == '__main__':
  
