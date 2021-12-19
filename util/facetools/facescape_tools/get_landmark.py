93d1e6725cccb29bd6740b198d4ad7b51c0871ff
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2021-12-19 10:25:24 -0500

    g

--- util/facetools/facescape_tools/get_landmark.py
+++ util/facetools/facescape_tools/get_landmark.py
@@ -53,47 +53,47 @@ if __name__ == '__main__':
     Rt_scale_dict = json.load(open("/home/uss00022/lelechen/github/lighting/predef/Rt_scale_dict.json", 'r'))
     lm_list_v10 = np.load("/home/uss00022/lelechen/github/lighting/predef/landmark_indices.npz")['v10']
     
-    for id_idx in tqdm(range(160,400)):
+    for id_idx in tqdm(range(1,400)):
         for exp_idx in range(1,21):
+            try:
+                img_dir = f"{image_data_root}/{id_idx}/{expressions[exp_idx]}"
+                with open(f"{img_dir}/params.json", 'r') as f:
+                    params = json.load(f)
+                print(f"Working on id={id_idx}, exp={exp_idx}")
 
-            img_dir = f"{image_data_root}/{id_idx}/{expressions[exp_idx]}"
-            with open(f"{img_dir}/params.json", 'r') as f:
-                params = json.load(f)
-            print(f"Working on id={id_idx}, exp={exp_idx}")
+                scale = Rt_scale_dict['%d'%id_idx]['%d'%exp_idx][0]
+                Rt_TU = np.array(Rt_scale_dict['%d'%id_idx]['%d'%exp_idx][1])
 
-            scale = Rt_scale_dict['%d'%id_idx]['%d'%exp_idx][0]
-            Rt_TU = np.array(Rt_scale_dict['%d'%id_idx]['%d'%exp_idx][1])
-        
-            # extract landmarks in canonical space
-            mesh_path = f"{mesh_root}/{id_idx}/models_reg/{expressions[exp_idx]}.obj"
-            om_mesh = openmesh.read_trimesh(mesh_path)
-            verts = np.array(om_mesh.points())
-            lmks = verts[lm_list_v10,:] # (68,3), in canonical space
+                # extract landmarks in canonical space
+                mesh_path = f"{mesh_root}/{id_idx}/models_reg/{expressions[exp_idx]}.obj"
+                om_mesh = openmesh.read_trimesh(mesh_path)
+                verts = np.array(om_mesh.points())
+                lmks = verts[lm_list_v10,:] # (68,3), in canonical space
 
-            # transform to world space
-            lmks = (np.tensordot(Rt_TU[:3,:3].T, (lmks - Rt_TU[:3, 3]).T, 1)).T
-            lmks /= scale # (68, 3), in world space
+                # transform to world space
+                lmks = (np.tensordot(Rt_TU[:3,:3].T, (lmks - Rt_TU[:3, 3]).T, 1)).T
+                lmks /= scale # (68, 3), in world space
 
-            imgs = glob.glob(f"{img_dir}/*.jpg")
-            landmark_dir = f"{landmark_root}/{id_idx}/{expressions[exp_idx]}"
-            os.makedirs(landmark_dir, exist_ok = True)
-            for img in imgs:
-                cam_idx = int(os.path.basename(img).split(".")[0])
+                imgs = glob.glob(f"{img_dir}/*.jpg")
+                landmark_dir = f"{landmark_root}/{id_idx}/{expressions[exp_idx]}"
+                os.makedirs(landmark_dir, exist_ok = True)
+                for img in imgs:
+                    cam_idx = int(os.path.basename(img).split(".")[0])
 
-                # projection to screen space
-                K = np.array(params['%d_K' % cam_idx])
-                Rt = np.array(params['%d_Rt' % cam_idx])
-                dist = np.array(params['%d_distortion' % cam_idx], dtype = np.float)
-                h_src = params['%d_height' % cam_idx]
-                w_src = params['%d_width' % cam_idx]
-                R = Rt[:3,:3]
-                T = Rt[:3,3:]
-                pos = K @ (R @ lmks.T + T)
-                coord = pos[:2,:] / pos[2,:] # (2, 68)
+                    # projection to screen space
+                    K = np.array(params['%d_K' % cam_idx])
+                    Rt = np.array(params['%d_Rt' % cam_idx])
+                    dist = np.array(params['%d_distortion' % cam_idx], dtype = np.float)
+                    h_src = params['%d_height' % cam_idx]
+                    w_src = params['%d_width' % cam_idx]
+                    R = Rt[:3,:3]
+                    T = Rt[:3,3:]
+                    pos = K @ (R @ lmks.T + T)
+                    coord = pos[:2,:] / pos[2,:] # (2, 68)
 
-                coord = np.transpose(coord, (1, 0))
-                
-                np.save( os.path.join( landmark_dir, '%d.npy'%cam_idx), coord)
+                    coord = np.transpose(coord, (1, 0))
+                    
+                    np.save( os.path.join( landmark_dir, '%d.npy'%cam_idx), coord)
                 # # plot test
                 # img = cv2.imread( os.path.join(  img_dir , "%d.jpg" % cam_idx ))
                 # # undist_img = cv2.undistort(img, K, dist)
@@ -108,6 +108,8 @@ if __name__ == '__main__':
                 #                             color = (255, 255, 255))
 
                 # cv2.imwrite( os.path.join(landmark_dir, "%d.jpg" % cam_idx )  , color_draw)
-
+            except:
+                print ('++++')
+                continue
 
   
