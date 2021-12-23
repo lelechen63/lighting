b23f860e49cd6fd10a66fe7dcc4ae198f254bf6e
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2021-12-22 19:08:57 -0500

    g

--- model/legacy.py
+++ model/legacy.py
@@ -12,7 +12,7 @@ import re
 import copy
 import numpy as np
 import torch
-import .dnnlib
+import . import dnnlib
 from torch_utils import misc
 
 #----------------------------------------------------------------------------
